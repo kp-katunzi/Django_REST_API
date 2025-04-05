cf874976504e40209b3a4e4f78e9a78bd8cfb1b8
kp-katunzi <paulkatunzi10@gmail.com>
2025-04-05 21:35:02 +0300

    model change

--- students/management/commands/seed.py
+++ students/management/commands/seed.py
@@ -11,38 +11,38 @@ class Command(BaseCommand):
 
         # Add Students
         students = [
-            {"name": "Alice Allen", "program": "Software Engineering"},
-            {"name": "Bob Jck", "program": "Data Science"},
-            {"name": "Charlie Charles", "program": "Cyber Security"},
-            {"name": "David Daniel", "program": "Software Engineering"},
-             {"name": "John bakari", "program": "Multmedia"},
-            {"name": "Bob", "program": "Information science"},
-            {"name": "Charles paul", "program": "Cyber Security Engineering"},
-            {"name": "Davidson Downson", "program": "Software Engineering"},
-            {"name": "kp paul", "program": "Cyber Security Engineering"},
-            {"name": "Davies Downson", "program": "Software Engineering"},
+            {"Student_name": "Alice Allen", "program": "Software Engineering"},
+            {"Student_name": "Bob Jck", "program": "Data Science"},
+            {"Student_name": "Charlie Charles", "program": "Cyber Security"},
+            {"Student_name": "David Daniel", "program": "Software Engineering"},
+             {"Student_name": "John bakari", "program": "Multmedia"},
+            {"Student_name": "Bob", "program": "Information science"},
+            {"Student_name": "Charles paul", "program": "Cyber Security Engineering"},
+            {"Student_name": "Davidson Downson", "program": "Software Engineering"},
+            {"Student_name": "kp paul", "program": "Cyber Security Engineering"},
+            {"Student_name": "Davies Downson", "program": "Software Engineering"},
         ]
         for student in students:
             Student.objects.create(**student)
 
         # Add Subjects
         subjects = [
-            {"name": "Intro to Programming", "year": 1},
-            {"name": "Data Structures", "year": 2},
-            {"name": "Databases", "year": 3},
-            {"name": "Machine Learning", "year": 4},
-             {"name": "Intro to Nertworking", "year": 1},
-            {"name": "Intro php ", "year": 2},
-            {"name": "Spring boot", "year": 3},
-            {"name": "Artificial Intelligence", "year": 4},
-             {"name": "Intro Software", "year": 1},
-            {"name": "computer theory", "year": 2},
-            {"name": "Android", "year": 3},
-            {"name": "Cloud computing", "year": 4},
-            {"name": "Intro to Software", "year": 1},
-            {"name": "computer theory", "year": 2},
-            {"name": "Linux", "year": 3},
-            {"name": "System Development", "year": 4},
+            {"course_name": "Intro to Programming", "year": 1},
+            {"course_name": "Data Structures", "year": 2},
+            {"course_name": "Databases", "year": 3},
+            {"course_name": "Machine Learning", "year": 4},
+            {"course_name": "Intro to Nertworking", "year": 1},
+            {"course_name": "Intro php ", "year": 2},
+            {"course_name": "Spring boot", "year": 3},
+            {"course_name": "Artificial Intelligence", "year": 4},
+            {"course_name": "Intro Software", "year": 1},
+            {"course_name": "computer theory", "year": 2},
+            {"course_name": "Android", "year": 3},
+            {"course_name": "Cloud computing", "year": 4},
+            {"course_name": "Intro to Software", "year": 1},
+            {"course_name": "computer theory", "year": 2},
+            {"course_name": "Linux", "year": 3},
+            {"course_name": "System Development", "year": 4},
         ]
         for subject in subjects:
             Subject.objects.create(**subject)
