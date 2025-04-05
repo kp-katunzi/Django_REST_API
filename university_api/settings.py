3b594d5207e862d3754eca9164a37374c7d7c797
kp-katunzi <paulkatunzi10@gmail.com>
2025-04-06 00:57:08 +0300

    setting changes

--- university_api/settings.py
+++ university_api/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'university_db', 
         'USER': 'root',    
-        'PASSWORD': 'Aaronpaul@100',  
+        'PASSWORD': 'Aaronpaul&100',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',       
     }
